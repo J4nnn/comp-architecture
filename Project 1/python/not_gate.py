e0aba9d9fe2ca605fa6f5ae6b966d9576ff6e0d3
Janer Vega <janervega399@gmail.com>
2025-02-11 11:20:39 -0500

    [Added] Reference to GfG

--- Project 1/python/not_gate.py
+++ Project 1/python/not_gate.py
@@ -8,14 +8,17 @@ def not_gate(input):
     input: True or False, 1 or 0. They must be the same value.
 
     Returns:
-    True or False, 1 or 0 according to the following truth table:
+    True or False according to the following truth table:
     | a | q |
     | 0 | 1 |
     | 1 | 0 |
     """
+
     return nand_gate(input, input)
 
-# Testing some examples
-print(not_gate(1))
-print(not_gate(0))
-print(not_gate("a"))
+# Testing some examples. (It's working properly)
+# print(not_gate(1))
+# print(not_gate(0))
+# print(not_gate("a"))
+
+# Reference: https://www.geeksforgeeks.org/implementation-of-not-gate-using-nand-gate/
